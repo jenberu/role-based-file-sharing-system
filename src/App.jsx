ebd0ab19d3b0b713a1b335963a803eec19aa9d9a
jenberu <jemberu0970@gmail.com>
2025-06-18 21:41:37 +0300

    Refactor routing in App component and add UserList component for user management

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPge from "./pages/HomePage";
 import NotFound from "./pages/NotFoundPage";
 import Login from "./pages/auth/Login";
-import DashboardLayout from './pages/DashboardLayout'
+import DashboardLayout from "./pages/DashboardLayout";
+import DashBoard from "./components/layout/dashboard";
 
 function App() {
-
   return (
     <>
-     <Router>
+      <Router>
         <Routes>
           {/* <Route path="/user/register" element={<SignUp />} />
-          */}
+           */}
           <Route path="/" element={<LandingPge />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard/*" element={<DashboardLayout />} />          
+          <Route element={<DashboardLayout />}>
+            <Route path="/dashboard/*" element={<DashBoard />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
-         </Routes>
-
-          </Router>
-
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
